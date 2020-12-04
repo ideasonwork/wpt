bc61bbf41de7ec6051e5570c5f30104f20b55977
Ben Kelly <wanderview@chromium.org>
2020-12-04 07:19:31 -0800

    URLPattern: Refactor WPT tests to match URL WPT test structure.
    The URL API tests use a data file that is loaded in the the test runner:
    
    https://github.com/web-platform-tests/wpt/blob/master/url/resources/urltestdata.json
    https://github.com/web-platform-tests/wpt/blob/master/url/url-constructor.html
    
    Purportedly this helps with other platforms that may want to implement
    the API maintain test parity by writing their own test runner around the
    data file.
    
    This seems like a good idea since we will likely need to test a polyfill
    in the future and it could also be used if nodejs adopts the API.
    
    This CL is roughly a direct translation of the current test cases.
    
    Bug: 1141510
    Change-Id: I6cf7dea233fed7751306822738fe86ec3464368e

---
+++ urlpattern/urlpattern.https.any.js
@@ -0,0 +1,17 @@
+// META: global=window,worker
+// META: script=resources/utils.js
+
+function runTests(data) {
+  for (let entry of data) {
+    test(function() {
+      const pattern = new URLPattern(entry.pattern);
+      assert_equals(pattern.test(entry.input), entry.expected);
+    }, `Pattern: ${JSON.stringify(entry.pattern)} Input: ${JSON.stringify(entry.input)}`);
+  }
+}
+
+promise_test(async function() {
+  const response = await fetch('resources/urlpatterntestdata.json');
+  const data = await response.json();
+  runTests(data);
+}, 'Loading data...');
